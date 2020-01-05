103d62259ce2628fe5fcda192d42b8a389ec80d0
EvonX <xiangchnchn@gmail.com>
2020-01-05 02:55:25 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TimeChangingDecisionTree", 
+    name="tcdt", 
     version="0.1",
     author="0xCC",
     description="Implementation of a Time-Changing Decision Tree classifier",
